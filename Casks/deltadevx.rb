a90f6d5484fc9479e0c8bae6fd57f4657e98cda9
rajharsh1997 <rajharsh1997@gmail.com>
2026-04-05 20:12:43 +0530

    fix : fixed structure

--- Casks/deltadevx.rb
+++ Casks/deltadevx.rb
@@ -1,9 +1,7 @@
 cask "deltadevx" do
-  arch arm: "aarch64"
-  
   version "1.0.0"
   sha256 "7e0b91187d760e62ce9ed9c842d40f14d61105b600dd7505b0366e059c1589c0"
-  url "https://github.com/rajharsh1997/deltadevx/releases/download/v#{version}/DeltaDevX_#{version}_#{arch}.dmg"
+  url "https://github.com/rajharsh1997/deltadevx/releases/download/v#{version}/DeltaDevX_#{version}_aarch64.dmg"
   name "DeltaDevX"
   desc "A cross-platform offline developer toolbox"
   homepage "https://github.com/rajharsh1997/deltadevx"
