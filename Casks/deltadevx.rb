03b3c201bce82217cc42c1cfabc971738f594e76
rajharsh1997 <rajharsh1997@gmail.com>
2026-05-05 19:23:37 +0530

    feat : update sha and url for DeltaDevX cask

--- Casks/deltadevx.rb
+++ Casks/deltadevx.rb
@@ -1,7 +1,7 @@
 cask "deltadevx" do
   version "1.1.0"
-  sha256 "d2b5de662c0634e03607d854fa415676396df6c097b7a70fa3d51a22785db5e1"
-  url "https://github.com/rajharsh1997/deltadevx/releases/download/v#{version}/DeltaDevX_#{version}_aarch64.dmg"
+  sha256 "4d4afaeb0e85c599ee228ef290bb3c12ce86415c8ea528b600838c2371dcea0b"
+  url "https://github.com/rajharsh1997/deltadevx/releases/download/v#{version}/DeltaDevX_#{version}_universal.dmg"
   name "DeltaDevX"
   desc "A cross-platform offline developer toolbox"
   homepage "https://github.com/rajharsh1997/deltadevx"
